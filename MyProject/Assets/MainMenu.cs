62de22962e361d559e81a4067bcca0e34f1367e4
dpoppe7 <70035439+dpoppe7@users.noreply.github.com>
2022-02-08 14:15:07 -0600

    inti

--- MyProject/Assets/MainMenu.cs
+++ MyProject/Assets/MainMenu.cs
@@ -1,29 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+
 
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         
     }
 
-    public void HowToPlay(){
-        SceneManager.LoadScene("Scene1");
+    public void Tutoria(){
+        //SceneManager.LoadScene("Scene1");
     }
     
 
     public void Play(){
-        SceneManager.LoadScene("Scene1");   
+        //SceneManager.LoadScene("Scene1");   
     }
 
     public void QuitButton(){
